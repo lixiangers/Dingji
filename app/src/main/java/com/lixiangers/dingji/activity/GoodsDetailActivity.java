f9ab975bc36c20d7fe94d72f4f198870526eeb90
lixiang <lixiang@qnwanwei.com>
2014-08-20 23:11:02 +0800

    [lixiang]adjust goods detail activity view

--- app/src/main/java/com/lixiangers/dingji/activity/GoodsDetailActivity.java
+++ app/src/main/java/com/lixiangers/dingji/activity/GoodsDetailActivity.java
@@ -8,13 +8,18 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.lixiangers.dingji.R;
 import com.lixiangers.dingji.dao.Goods;
 import com.lixiangers.dingji.util.Constant;
+import com.lixiangers.dingji.util.DensityUtil;
 import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class GoodsDetailActivity extends NeolixNaviagationBaseActivity {
 
@@ -25,6 +30,9 @@ public class GoodsDetailActivity extends NeolixNaviagationBaseActivity {
     private Button byButton;
     private TextView goodsPriceTextView;
     private TextView goodsDesTextView;
+    private LinearLayout pointsView;
+    private List<ImageView> pointsImageList;
+    private int currentIndex = 0;
 
     public GoodsDetailActivity() {
         super(R.layout.activity_detail);
@@ -35,17 +43,31 @@ public class GoodsDetailActivity extends NeolixNaviagationBaseActivity {
         super.onCreate(savedInstanceState);
         goods = (Goods) getIntent().getSerializableExtra(Constant.GOODS);
         setTitle(goods.getName());
+        pointsImageList = new ArrayList<ImageView>();
 
         initView();
         initViewpager();
         initListener();
+        viewPager.setCurrentItem(0);
+        setCurrentDot(currentIndex);
     }
 
     private void initView() {
         goodsDesTextView = (TextView) findViewById(R.id.tv_goods_des);
         goodsPriceTextView = (TextView) findViewById(R.id.tv_goods_price);
         byButton = (Button) findViewById(R.id.bt_by);
+        pointsView = (LinearLayout) findViewById(R.id.points_view);
 
+        int images = goods.getImageArrayList().isEmpty() ? 1 : goods.getImageArrayList().size();
+        for (int i = 0; i < images; i++) {
+            ImageView imageView = new ImageView(getApplicationContext());
+            imageView.setImageResource(R.drawable.selector_point);
+            imageView.setEnabled(false);
+            int padding = DensityUtil.dip2px(getApplicationContext(), 15f);
+            imageView.setPadding(padding, padding, padding, padding);
+            pointsView.addView(imageView);
+            pointsImageList.add(imageView);
+        }
         goodsDesTextView.setText(goods.getDes());
         goodsPriceTextView.setText(goods.getPriceAndUnit());
     }
@@ -73,6 +95,29 @@ public class GoodsDetailActivity extends NeolixNaviagationBaseActivity {
 
         viewPager = (ViewPager) findViewById(R.id.view_pager);
         viewPager.setAdapter(new SamplePagerAdapter());
+
+        viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int i, float v, int i2) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                setCurrentDot(position);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int i) {
+            }
+        });
+    }
+
+    private void setCurrentDot(int position) {
+        pointsImageList.get(currentIndex).setEnabled(false);
+        pointsImageList.get(position).setEnabled(true);
+
+        currentIndex = position;
     }
 
     class SamplePagerAdapter extends PagerAdapter {
