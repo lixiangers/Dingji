a02d76b28e458770a694fa9036309960bab9e43e
lixiang <lixiang@neolix.com>
2014-09-05 12:25:09 +0800

    [liixang]add menu

--- app/src/main/java/com/lixiangers/dingji/activity/ShoppingCartFragment.java
+++ app/src/main/java/com/lixiangers/dingji/activity/ShoppingCartFragment.java
@@ -1,16 +1,19 @@
 package com.lixiangers.dingji.activity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
-import android.view.ContextMenu;
+import android.view.Gravity;
 import android.view.LayoutInflater;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 
 import com.lixiangers.dingji.R;
@@ -26,13 +29,16 @@ import java.util.List;
 
 import static com.lixiangers.dingji.util.StringUtil.showText;
 
-public class ShoppingCartFragment extends android.support.v4.app.Fragment {
+public class ShoppingCartFragment extends android.support.v4.app.Fragment implements View.OnClickListener {
     private static List<ShoppingItem> shoppingItemList = new ArrayList<ShoppingItem>();
     private ModelListAdapter<ShoppingItem> adapter;
     private ListView goodsListView;
     private TextView totalAmountTextView;
     private Button commitButton;
     private int totalAmount;
+    private PopupWindow popupWindow;
+    private View mPopupWindowView;
+    private int currentItemIndex = -1;
 
     public static void addGoods(ShoppingItem item) {
         boolean isExits = false;
@@ -68,6 +74,15 @@ public class ShoppingCartFragment extends android.support.v4.app.Fragment {
 
         adapter = new ModelListAdapter<ShoppingItem>(MyApplication.getInstance());
         goodsListView.setAdapter(adapter);
+        goodsListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                currentItemIndex = position;
+                initPopupWindow(adapter.getItem(position).getGoods().getName());
+                showPopupWindow();
+                return false;
+            }
+        });
 
         addNumberChangeListener();
         commitButton.setOnClickListener(new View.OnClickListener() {
@@ -85,29 +100,6 @@ public class ShoppingCartFragment extends android.support.v4.app.Fragment {
         return view;
     }
 
-    @Override
-    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
-        MenuInflater inflater = new MenuInflater(getActivity());
-        inflater.inflate(R.menu.menu, menu);
-        super.onCreateContextMenu(menu, v, menuInfo);
-    }
-
-    @Override
-    public boolean onContextItemSelected(MenuItem item) {
-        item.setChecked(true);
-
-        final AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
-        switch (item.getItemId()) {
-            case R.id.delete_menu:
-                if (info != null) {
-                    shoppingItemList.remove(info.position);
-                    loadData();
-                }
-                break;
-        }
-        return super.onContextItemSelected(item);
-    }
-
     private void loadData() {
         adapter.setData(shoppingItemList);
         goodsListView.setVisibility(shoppingItemList.isEmpty() ? View.GONE : View.VISIBLE);
@@ -122,10 +114,10 @@ public class ShoppingCartFragment extends android.support.v4.app.Fragment {
                 @Override
                 public void onNumberChange(int number) {
                     shoppingItem.setQuantity(number);
-//                    if (number == 0) {
-//                        orderItemList.remove(orderItem);
-//                        loadData();
-//                    }
+                    if (number == 0) {
+                        shoppingItemList.remove(shoppingItem);
+                        loadData();
+                    }
 
                     calcTotalAmount();
                 }
@@ -139,5 +131,46 @@ public class ShoppingCartFragment extends android.support.v4.app.Fragment {
             totalAmount += shoppingItem.getTotalAmount();
         }
         totalAmountTextView.setText(String.format("%.2f", totalAmount / 100f));
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.lv_menu:
+                if (currentItemIndex > -1) {
+                    shoppingItemList.remove(currentItemIndex);
+                    loadData();
+                }
+                popupWindow.dismiss();
+                break;
+        }
+    }
+
+    private void initPopupWindow(String title) {
+        initPopupWindowView(title);
+        popupWindow = new PopupWindow(mPopupWindowView, WindowManager.LayoutParams.WRAP_CONTENT, WindowManager.LayoutParams.WRAP_CONTENT);
+
+        popupWindow.setTouchable(true);
+        popupWindow.setOutsideTouchable(true);
+        popupWindow.setBackgroundDrawable(new BitmapDrawable(getResources(), (Bitmap) null));
+
+        popupWindow.setAnimationStyle(R.style.my_popwindow_anim_style);
+        popupWindow.update();
+    }
+
+    private void initPopupWindowView(String title) {
+        mPopupWindowView = LayoutInflater.from(getActivity()).inflate(R.layout.menu_frgment_pickup, null);
+        LinearLayout layout = (LinearLayout) mPopupWindowView.findViewById(R.id.lv_menu);
+        layout.setOnClickListener(this);
+        TextView titleTextView = (TextView) mPopupWindowView.findViewById(R.id.tv_title);
+        titleTextView.setText(title);
+    }
+
+    private void showPopupWindow() {
+        if (!popupWindow.isShowing()) {
+            popupWindow.showAtLocation(ShoppingCartFragment.this.getView(), Gravity.CENTER, 0, 0);
+        } else {
+            popupWindow.dismiss();
+        }
     }
 }
